9dbfe38970b4bb886f9ef621ebacf8a26a9505c9
doomdonut666 <doomdonut666@gmail.com>
2026-05-29 08:21:20 +0900

    changed icons

--- scripts/inputScript.js
+++ scripts/inputScript.js
@@ -2,6 +2,7 @@ const form = document.querySelector('.top-banner form');
 const apiKey = "3727298d5f718b4abe846f814a39ada9";
 const input = document.querySelector('input');
 const msg = document.querySelector('.msg')
+const list = document.querySelector('.cities');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -17,10 +18,39 @@ form.addEventListener('submit', async (e) => {
       throw new Error("City not found :(");
     }
 
+    // getting json data from response
     const data = await response.json();
-    console.log(data);
-    msg.textContent = data;
+    const { main, name, sys, weather } = data;
+
+    // getting icon via url, using icons from Envato
+    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]
+      }.svg`;
+
+    // creating li element with data we got
+    const li = document.createElement("li");
+    li.classList.add("city");
+    const markup = ` 
+    <h2 class="city-name" data-name="${name},${sys.country}"> 
+    <span>${name}</span> 
+    <sup>${sys.country}</sup> 
+    </h2> 
+    <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup> 
+    </div> 
+    <figure> 
+    <img class="city-icon" src=${icon} alt=${weather[0]["main"]}> 
+    <figcaption>${weather[0]["description"]}</figcaption> 
+    </figure> 
+`;
+    li.innerHTML = markup;
+    list.appendChild(li);
+
+    console.log(data); // I'll remove it later
+    msg.textContent = 'Done :)';
   } catch (error) {
     msg.textContent = 'Please enter valid city :(';
+    console.log(error);
   }
+
+  form.reset();
+  input.focus();
 });
