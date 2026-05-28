f0ca54c3d52fcb19722586a2bef42432e8424f94
doomdonut666 <doomdonut666@gmail.com>
2026-05-29 06:44:44 +0900

    rewrited input script

--- scripts/inputScript.js
+++ scripts/inputScript.js
@@ -3,13 +3,24 @@ const apiKey = "3727298d5f718b4abe846f814a39ada9";
 const input = document.querySelector('input');
 const msg = document.querySelector('.msg')
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
   const inputValue = input.value;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`;
+
   console.log(url);
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => msg.textContent = 'Please enter valid city :(')
-})
+  try {
+    const response = await fetch(url);
+
+    // Response status check
+    if (!response.ok) {
+      throw new Error("City not found :(");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    msg.textContent = data;
+  } catch (error) {
+    msg.textContent = 'Please enter valid city :(';
+  }
+});
